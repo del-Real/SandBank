dacaa6083d0a5c0f7a040529d50cae56c0f3a236
Alberto del Real <theredsantar@gmail.com>
2026-02-26 20:05:27 +0100

    init

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UsersController(IUserService userService)
+    {
+        _userService = userService;
+    }
+}
---
+++ User.cs
@@ -0,0 +1,5 @@
+public class User
+{
+    public int IdUser { get; set; }
+    public int Name { get; set; }
+}
